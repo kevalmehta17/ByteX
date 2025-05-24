01930c4c825b60445c459e4d8854183a085d12b2
Keval <kevalmehta885@gmail.com>
2025-05-24 20:41:18 +0530

    feat: User Model Schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    bio: {
+        type: String,
+        default: "",
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+    nativeLanguage: {
+        type: String,
+        default: "",
+    },
+    learningLanguage: {
+        type: String,
+        default: "",
+    },
+    location: {
+        type: String,
+        default: "",
+    },
+    isOnboarded: {
+        type: Boolean,
+        default: false,
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+}, { timestamps: true });
+
+
+const User = mongoose.model("User", userSchema);
+
+//Pre-hook to hash password before saving
+
+userSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        console.error("Error hashing password:", error);
+        next(error);
+
+    }
+})
+
+export default User; 
